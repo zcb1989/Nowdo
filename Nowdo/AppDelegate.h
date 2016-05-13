b5604ab0ce2b386f4b84faa9e0147962c6f8ab6f
anakin <anakin@tangodembp.lan>
2016-05-13 09:20:28 +0800

    Initial Commit

---
+++ Nowdo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nowdo
+//
+//  Created by anakin on 16/5/13.
+//  Copyright © 2016年 Leou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nowdo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nowdo
+//
+//  Created by anakin on 16/5/13.
+//  Copyright © 2016年 Leou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
